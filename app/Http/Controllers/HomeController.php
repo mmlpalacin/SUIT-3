e284b48edd066a86d3d84832050c381c71b98057
mmlpalacin <milagrosp.urena@gmail.com>
2024-08-08 18:33:14 -0300

    dashboard

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,9 +6,6 @@ use Illuminate\Http\Request;
 use App\Models\Anuncio;
 use App\Models\User;
 use Spatie\Permission\Models\Role;
-use Illuminate\Support\Facades\Auth;
-use App\Models\Asignacion_de_Cursos;
-use App\Models\Asistencia;
 
 class HomeController extends Controller
 {
@@ -21,17 +18,5 @@ class HomeController extends Controller
 
         $anuncios = Anuncio::where('status', 2)->whereIn('user_id', $users->pluck('id'))->latest('id')->paginate();
         return view ('welcome', compact('anuncios'));
-    }
-    public function dashboard()
-    {
-        $user = Auth::user();
-        $role= Auth::user()->roles->first()->name;
-
-        $cursos = Asignacion_de_Cursos::where('user_id', $user->id)->with('curso')->get();
-
-        $asistencias = Asistencia::whereHas('alumno', function ($query) use ($user) {
-            $query->where('alumno_id', $user->id);
-        })->get();
-        return view('dashboard', compact('cursos', 'asistencias', 'role'));
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use Illuminate\Support\Facades\Auth;
+use App\Models\Asignacion_de_Cursos;
+use App\Models\Asistencia;
 
 Route::get('/', [HomeController::class, 'index'])->name('anuncios.index');
 
@@ -10,5 +13,16 @@ Route::middleware([
     config('jetstream.auth_session'),
     'verified',
 ])->group(function () {
-    Route::get('/perfil', [HomeController::class, 'dashboard'])->name('dashboard');
+    Route::get('/dashboard', function () {
+        $user = Auth::user();
+        $role = $user->roles->first()->name;
+
+        $cursos = Asignacion_de_Cursos::where('user_id', $user->id)->with('curso')->get();
+        
+        $asistencias = Asistencia::whereHas('alumno', function ($query) use ($user) {
+            $query->where('alumno_id', $user->id);
+        })->get();
+
+        return view('dashboard', compact('cursos', 'asistencias', 'role'));
+    })->name('dashboard');
 });
